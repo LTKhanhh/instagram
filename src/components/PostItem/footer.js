583bcd5dd041ebc72c654974fa0f97d0e49f1623
22026531-LeTrongKhanh <22026531@vnu.edu.vn>
2024-06-21 13:34:55 +0700

    post

---
+++ src/components/PostItem/footer.js
@@ -0,0 +1,47 @@
+import classNames from "classnames/bind";
+import { useState, useRef, useEffect } from "react";
+
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faEllipsis, faComment } from '@fortawesome/free-solid-svg-icons';
+import styles from './PostItem.module.scss'
+
+const cx = classNames.bind(styles)
+
+function Pfooter({ handleSubmit }) {
+    const [text, setText] = useState('');
+    const isTextareaDisabled = text.trim().length !== 0;
+    const onSubmit = (event) => {
+        event.preventDefault();
+        handleSubmit(text.trim());
+        setText("");
+    };
+    const textareaRef = useRef(null);
+
+    return (
+        <div className={cx('f-container')}>
+            <div className={cx('font-bold', 'mb-2')} style={{ fontSize: '14px' }}>19.750 lượt thích</div>
+            <div className={cx('status-container', 'flex')} style={{ fontSize: '14px' }}>
+                <div className={cx('info-user', 'font-bold', 'mr-2')}>ltk_cmbny</div>
+                <div className={cx('status')}>alo 1234</div>
+            </div>
+            <div className={cx('view-cmt')}>
+                <span style={{ color: '#999', fontSize: '14px' }}>Xem tất cả 1000 bình luận</span>
+            </div>
+
+            <form className={cx('cmt-container', 'flex')} onSubmit={onSubmit}>
+                <textarea
+                    ref={textareaRef}
+                    className={cx('custom-scrollbar-textarea', 'input')}
+                    value={text}
+                    onChange={(e) => setText(e.target.value)}
+                    placeholder="Thêm bình luận..."
+                />
+                {isTextareaDisabled && (
+                    <span>Đăng</span>
+                )}
+            </form >
+        </div>
+    );
+}
+
+export default Pfooter;
--- src/components/PostItem/index.js
+++ src/components/PostItem/index.js
@@ -1,8 +1,8 @@
 import classNames from "classnames/bind";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEllipsis } from '@fortawesome/free-solid-svg-icons';
+import { faEllipsis, faComment } from '@fortawesome/free-solid-svg-icons';
 import styles from './PostItem.module.scss'
-
+import Pfooter from "./footer";
 const cx = classNames.bind(styles)
 
 function PostItem() {
@@ -29,16 +29,115 @@ function PostItem() {
                     <img src={`${process.env.PUBLIC_URL}/we.jpg`}></img>
                 </div>
 
-                <div className={cx('action')}>
+                <div className={cx('action', 'flex', 'justify-between')}>
                     <div className={cx('action-left', 'flex')}>
                         <span className={cx('action-item')}>
                             <img src={`${process.env.PUBLIC_URL}/heart-regular.svg`}></img>
                         </span>
+                        <span className={cx('action-item')}>
+                            <img src={`${process.env.PUBLIC_URL}/comment-regular.svg`}></img>
+                        </span>
+                        <span className={cx('action-item')}>
+                            <img src={`${process.env.PUBLIC_URL}/paper-plane-regular.svg`}></img>
+                        </span>
                     </div>
-                    <div className={cx('action-right')}></div>
+                    <div className={cx('action-right')}>
+                        <span className={cx('action-item')}>
+                            <img className="w-full h-full" src={`${process.env.PUBLIC_URL}/bookmark-regular.svg`}></img>
+                        </span>
+                        {/* <div className={cx('hollow-triangle')}></div> */}
+                    </div>
                 </div>
+
+                <Pfooter />
             </div>
-        </div>
+            <div className={cx('content')}>
+                <div className={cx('flex', 'info', 'items-center', 'justify-between')}>
+                    <div className={cx('info-left', 'flex', 'items-center')}>
+                        <div className={cx('info-user', 'flex')}>
+                            <span className={cx('avatar')}>
+                                <img src={`https://upload.wikimedia.org/wikipedia/commons/thumb/d/d7/Cristiano_Ronaldo_playing_for_Al_Nassr_FC_against_Persepolis%2C_September_2023_%28cropped%29.jpg/800px-Cristiano_Ronaldo_playing_for_Al_Nassr_FC_against_Persepolis%2C_September_2023_%28cropped%29.jpg`}></img>
+                            </span>
+                            <span className={cx('name')}>ltk_cmbny</span>
+                        </div>
+                        <div className={cx('time')}>
+                            <span>3 ngày</span>
+                        </div>
+                    </div>
+                    <div className={cx('info-right')}>
+                        <FontAwesomeIcon icon={faEllipsis}></FontAwesomeIcon>
+                    </div>
+                </div>
+                <div className={cx('image-container')}>
+                    <img src={`${process.env.PUBLIC_URL}/we.jpg`}></img>
+                </div>
+
+                <div className={cx('action', 'flex', 'justify-between')}>
+                    <div className={cx('action-left', 'flex')}>
+                        <span className={cx('action-item')}>
+                            <img src={`${process.env.PUBLIC_URL}/heart-regular.svg`}></img>
+                        </span>
+                        <span className={cx('action-item')}>
+                            <img src={`${process.env.PUBLIC_URL}/comment-regular.svg`}></img>
+                        </span>
+                        <span className={cx('action-item')}>
+                            <img src={`${process.env.PUBLIC_URL}/paper-plane-regular.svg`}></img>
+                        </span>
+                    </div>
+                    <div className={cx('action-right')}>
+                        <span className={cx('action-item')}>
+                            <img className="w-full h-full" src={`${process.env.PUBLIC_URL}/bookmark-regular.svg`}></img>
+                        </span>
+                        {/* <div className={cx('hollow-triangle')}></div> */}
+                    </div>
+                </div>
+
+                <Pfooter />
+            </div>
+            <div className={cx('content')}>
+                <div className={cx('flex', 'info', 'items-center', 'justify-between')}>
+                    <div className={cx('info-left', 'flex', 'items-center')}>
+                        <div className={cx('info-user', 'flex')}>
+                            <span className={cx('avatar')}>
+                                <img src={`https://upload.wikimedia.org/wikipedia/commons/thumb/d/d7/Cristiano_Ronaldo_playing_for_Al_Nassr_FC_against_Persepolis%2C_September_2023_%28cropped%29.jpg/800px-Cristiano_Ronaldo_playing_for_Al_Nassr_FC_against_Persepolis%2C_September_2023_%28cropped%29.jpg`}></img>
+                            </span>
+                            <span className={cx('name')}>ltk_cmbny</span>
+                        </div>
+                        <div className={cx('time')}>
+                            <span>3 ngày</span>
+                        </div>
+                    </div>
+                    <div className={cx('info-right')}>
+                        <FontAwesomeIcon icon={faEllipsis}></FontAwesomeIcon>
+                    </div>
+                </div>
+                <div className={cx('image-container')}>
+                    <img src={`${process.env.PUBLIC_URL}/we.jpg`}></img>
+                </div>
+
+                <div className={cx('action', 'flex', 'justify-between')}>
+                    <div className={cx('action-left', 'flex')}>
+                        <span className={cx('action-item')}>
+                            <img src={`${process.env.PUBLIC_URL}/heart-regular.svg`}></img>
+                        </span>
+                        <span className={cx('action-item')}>
+                            <img src={`${process.env.PUBLIC_URL}/comment-regular.svg`}></img>
+                        </span>
+                        <span className={cx('action-item')}>
+                            <img src={`${process.env.PUBLIC_URL}/paper-plane-regular.svg`}></img>
+                        </span>
+                    </div>
+                    <div className={cx('action-right')}>
+                        <span className={cx('action-item')}>
+                            <img className="w-full h-full" src={`${process.env.PUBLIC_URL}/bookmark-regular.svg`}></img>
+                        </span>
+                        {/* <div className={cx('hollow-triangle')}></div> */}
+                    </div>
+                </div>
+
+                <Pfooter />
+            </div>
+        </div >
     );
 }
 
